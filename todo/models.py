d0e009d1a28e3afc36fe1e01fc6b0d4c039e364f
sathish1131 <sathishkumar1131@gmail.com>
2024-09-08 11:21:26 +0530

    linked user with tasks

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class TodoList(models.Model):
+    priority_choices = [(1, 'Urjent'), (2, 'Normal'), (3, 'Optional')]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
     task = models.CharField(max_length=500)
+    priority = models.IntegerField(priority_choices, default=3)
--- todo/views.py
+++ todo/views.py
@@ -4,5 +4,5 @@ from .models import TodoList
 # Create your views here.
 
 def todo(request):
-    list = TodoList.objects.all()
+    list = TodoList.objects.filter(user = request.user)
     return render(request, 'todo.html', {'list': list})
