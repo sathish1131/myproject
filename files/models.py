245c1ad485a052a6f8285611591b9e5af0b78ef1
sathish1131 <sathishkumar1131@gmail.com>
2024-09-22 11:17:09 +0530

    files model done

--- files/models.py
+++ files/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class File(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default='')
     file = models.FileField(upload_to="files/")
+    date = models.DateTimeField(default=timezone.now)
