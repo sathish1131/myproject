ad1bee75b590215bc5a8ddea7b2066e33e2f9eea
sathish1131 <sathishkumar1131@gmail.com>
2024-09-22 20:52:16 +0530

    models for file app done

--- files/models.py
+++ files/models.py
@@ -6,6 +6,18 @@ from django.utils import timezone
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=100)
     file = models.FileField(upload_to="files/")
     date = models.DateTimeField()
+    parent_folder = models.ForeignKey('Folder', null=True, blank=True, on_delete=models.CASCADE, related_name='files')
+
+    def __str__(self):
+        return self.name
+
+class Folder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    parent_folder = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subfolders')
+
+    def __str__(self):
+        return self.name
--- files/views.py
+++ files/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import File
 from django.http import JsonResponse
 from django.utils import timezone
 
