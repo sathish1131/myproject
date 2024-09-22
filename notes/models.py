ee578c4c1011f2ad5e89da9e1b0c2fc98351d62e
sathish1131 <sathishkumar1131@gmail.com>
2024-09-23 01:13:39 +0530

    model for notes done

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    text = models.CharField(max_length=2000)
+    text = models.TextField()
+    updated_at = models.DateTimeField(default=timezone.now)
