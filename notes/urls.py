53afd1baac69ddc23218b27446219ba2713c69a9
sathish1131 <sathishkumar1131@gmail.com>
2024-09-27 02:56:01 +0530

    notes app done

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('notes', views.notes, name= "notes"),
-    path('save-note', views.save_note, name= "save_note"),
-    path('delete-note', views.delete_note, name= "delete_note")
+    path('save-note/', views.save_note, name= "save_note"),
+    path('delete-note/', views.delete_note, name= "delete_note"),
+    path('get-note/', views.get_note, name= "get_note")
 ]
--- notes/views.py
+++ notes/views.py
@@ -9,6 +9,15 @@ def notes(request):
     notes = Note.objects.filter(user=request.user).order_by('-updated_at')
     return render(request, 'notes.html', {'notes': notes})
 
+def get_note(request):
+    if(request.method == 'GET'):
+        note_id = request.GET.get('id')
+        note = get_object_or_404(Note, id=note_id, user=request.user)
+        return JsonResponse({
+            'name': note.name,
+            'content': note.text
+        })
+
 def delete_note(request):
     if request.method == 'POST':
         note_id = request.POST.get('id')
@@ -19,21 +28,19 @@ def delete_note(request):
 
 def save_note(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        note_content = request.POST.get('note')
-        response_data = {}
-        if name or note_content:
-            note, created = Note.objects.get_or_create(user=request.user, name=name, defaults={
-                'note': note_content,
-                'updated_at': timezone.now()
-            })
-            if created:
-                response_data['message'] = 'Note added successfully'
-            else:
-                note.name = name
-                note.note = note_content
-                note.updated_at = timezone.now()
-                note.save()
-                response_data['message'] = 'Note updated successfully'
-            return JsonResponse(response_data)
-    return JsonResponse({'status': 'failed', 'message': 'Failed to add or update note'})
+        note_id = request.POST.get('id')
+        note_name = request.POST.get('name')
+        note_content = request.POST.get('content')
+        if not note_name or not note_content:
+            return JsonResponse({'status': 'failed', 'message': 'Note name and content cannot be empty', 'status': 400})
+        if note_id:
+            note = get_object_or_404(Note, id=note_id, user=request.user)
+            note.name = note_name
+            note.text = note_content
+            note.updated_at = timezone.now()
+            note.save()
+        else:
+            note = Note(name= note_name, text= note_content, updated_at= timezone.now(), user= request.user)
+            note.save()
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'failed'})
