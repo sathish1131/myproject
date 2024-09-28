07241cfbbc48c312d9f0535893e6a5127057cb0c
sathish1131 <sathishkumar1131@gmail.com>
2024-09-28 16:12:19 +0530

    refined models for file app

--- files/models.py
+++ files/models.py
@@ -14,14 +14,12 @@ class Folder(models.Model):
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    unique_name = models.CharField(max_length=255, unique=True)
-    file = models.FileField(upload_to="uploads/")
-    updated_at = models.DateTimeField(auto_now_add=True)
     parent_folder = models.ForeignKey(Folder, null=True, blank=True, on_delete=models.CASCADE)
-    size = models.PositiveIntegerField()
-    content_type = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=100)
+    unique_name = models.CharField(max_length=255, unique=True)
+    url = models.URLField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.name
 
