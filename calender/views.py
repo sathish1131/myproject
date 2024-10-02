93c4727047d010a276b6096eb0a5d3db1e992d41
sathish1131 <sathishkumar1131@gmail.com>
2024-10-02 23:13:18 +0530

    calender app started

--- calender/views.py
+++ calender/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-import calender
+import calendar
+from calendar import HTMLCalendar
 import datetime
 
 # Create your views here.
 
 def calender(request):
     now = datetime.date.today()
-    return render(request, 'calender.html', {'calender': now})
+    cal = HTMLCalendar().formatmonth(now.year, now.month)
+    return render(request, 'calender.html', {'calender': cal})
