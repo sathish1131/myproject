033568cc27f9cb78d84ca0ce989c67d6e5436f85
sathish1131 <sathishkumar1131@gmail.com>
2024-10-06 11:33:38 +0530

    migrated to postgres from dqlite

--- myproject/settings.py
+++ myproject/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'satheesh',
+        'PASSWORD': 'kumar@1131',
+        'HOST': 'localhost'
     }
 }
 
