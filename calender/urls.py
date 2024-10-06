20768002c7555477aa59a01843fa9f85ab5b8231
sathish1131 <sathishkumar1131@gmail.com>
2024-10-06 20:53:48 +0530

    added event table dynamic

--- calender/urls.py
+++ calender/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('calender', views.calender, name= "calender"),
     path('fetch-calender/', views.fetch_calender, name="fetch_calender"),
-    path('fetch-years/', views.fetch_years, name="fetch_years")
+    path('fetch-years/', views.fetch_years, name="fetch_years"),
+    path('fetch_events/', views.fetch_events, name="fetch_events")
 ]
--- calender/views.py
+++ calender/views.py
@@ -3,13 +3,14 @@ from django.http import JsonResponse
 import calendar
 from calendar import HTMLCalendar
 import datetime
+from .models import Event
 
 # Create your views here.
 
 def calender(request):
     current_year = datetime.datetime.now().year
     current_month = datetime.datetime.now().month
-    current_day = datetime.datetime.now().day
+    current_date = datetime.datetime.now().day
     decade_start = current_year-((current_year%10)-1)
     current_decade = f"{decade_start}-{decade_start+9}"
     cal = HTMLCalendar().formatmonth(current_year, current_month)
@@ -19,7 +20,7 @@ def calender(request):
         end_year = start_year + 9
         decades.append(f"{start_year}-{end_year}")
     months = list(calendar.month_name[1:13])
-    return render(request, 'calender.html', {'calender': cal, 'current_month': current_month, 'current_year': current_year, 'current_day': current_day, 'current_decade': current_decade, 'months': months, 'years': years, 'decades': decades})
+    return render(request, 'calender.html', {'calender': cal, 'current_month': current_month, 'current_year': current_year, 'current_date': current_date, 'current_decade': current_decade, 'months': months, 'years': years, 'decades': decades})
 
 def fetch_calender(request):
     if request.method == 'GET':
@@ -36,3 +37,32 @@ def fetch_years(request):
         for year in range(int(split_decade[0]), int(split_decade[1])+1,1):
             years.append(year)
         return JsonResponse({'success': True, 'years': years})
+    
+def fetch_events(request):
+    if request.method == 'GET':
+        event_year = request.GET.get('event_year')
+        event_month = request.GET.get('event_month')
+        event_date = request.GET.get('event_date')
+        formated_date = datetime.date(int(event_year), int(event_month), int(event_date))
+        # events = Event.objects.filter(user=request.user, date=formated_date)
+        events = [
+            {
+                'id': 1,
+                'event': 'first test',
+                'start_time': '10:30',
+                'end_time': '11:30'
+            },
+                {
+                'id': 2,
+                'event': 'first test',
+                'start_time': '16:30',
+                'end_time': '17:30'
+            },
+                {
+                'id': 3,
+                'event': 'first test',
+                'start_time': '10:30',
+                'end_time': '11:30'
+            },
+        ]
+        return JsonResponse({'events': events})
