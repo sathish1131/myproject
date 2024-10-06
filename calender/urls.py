821cd6b1315c1dd37838b7bdceda5c3fb724114a
sathish1131 <sathishkumar1131@gmail.com>
2024-10-07 01:53:11 +0530

    event delete done

--- calender/urls.py
+++ calender/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('calender', views.calender, name= "calender"),
     path('fetch-calender/', views.fetch_calender, name="fetch_calender"),
     path('fetch-years/', views.fetch_years, name="fetch_years"),
-    path('fetch_events/', views.fetch_events, name="fetch_events")
+    path('fetch_events/', views.fetch_events, name="fetch_events"),
+    path('add-event/', views.add_event, name="add_event"),
+    path('edit-event/', views.edit_event, name="edit_event"),
+    path('delete-event/', views.delete_event, name="delete_event")
 ]
--- calender/views.py
+++ calender/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import calendar
 from calendar import HTMLCalendar
-import datetime
+from datetime import datetime
 from .models import Event
 
 # Create your views here.
 
 def calender(request):
-    current_year = datetime.datetime.now().year
-    current_month = datetime.datetime.now().month
-    current_date = datetime.datetime.now().day
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    current_date = datetime.now().day
     decade_start = current_year-((current_year%10)-1)
     current_decade = f"{decade_start}-{decade_start+9}"
     cal = HTMLCalendar().formatmonth(current_year, current_month)
@@ -43,26 +43,47 @@ def fetch_events(request):
         event_year = request.GET.get('event_year')
         event_month = request.GET.get('event_month')
         event_date = request.GET.get('event_date')
-        formated_date = datetime.date(int(event_year), int(event_month), int(event_date))
-        # events = Event.objects.filter(user=request.user, date=formated_date)
-        events = [
-            {
-                'id': 1,
-                'event': 'first test',
-                'start_time': '10:30',
-                'end_time': '11:30'
-            },
-                {
-                'id': 2,
-                'event': 'first test',
-                'start_time': '16:30',
-                'end_time': '17:30'
-            },
-                {
-                'id': 3,
-                'event': 'first test',
-                'start_time': '10:30',
-                'end_time': '11:30'
-            },
-        ]
-        return JsonResponse({'events': events})
+        formated_date = datetime(year=int(event_year),month=int(event_month), day=int(event_date)).date()
+        events = Event.objects.filter(user=request.user, date=formated_date)
+        events_data = list(events.values('id', 'event', 'start_time', 'end_time'))
+        return JsonResponse({'success':True, 'events': events_data})
+
+def delete_event(request):
+    if request.method == 'POST':
+        event_id = request.POST.get('id')
+        event = get_object_or_404(Event, user=request.user, id=event_id)
+        event.delete()
+        return JsonResponse({'success': True})
+
+def edit_event(request):
+    if request.method == 'POST':
+        event_id = request.POST.get('id')
+        event_event = request.POST.get('event')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+        event = get_object_or_404(Event, user=request.user, id=event_id)
+        event.event = event_event
+        event.start_time = datetime.strptime(start_time, '%H:%M')
+        event.end_time = datetime.strptime(end_time, '%H:%M')
+        event.save()
+        return JsonResponse({'success': True})
+    
+def add_event(request):
+    if request.method == 'POST':
+        event_event = request.POST.get('event')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+        event_year = request.POST.get('event_year')
+        event_month = request.POST.get('event_month')
+        event_date = request.POST.get('event_date')
+        formated_date = datetime(year=int(event_year),month=int(event_month), day=int(event_date)).date()
+        startTimeList = start_time.split(':')
+        endTimeList = end_time.split(':')
+        start_time_obj = datetime(hour=int(startTimeList[0]), minute=int(startTimeList[1])).time()
+        end_time_obj = datetime(hour=int(end_time[0]), minute=int(endTimeList[1])).time()
+        print(start_time_obj, end_time_obj)
+        event = Event(user=request.user, event=event_event, start_time=start_time_obj, end_time=end_time_obj, date=formated_date)
+        event.save()
+        return JsonResponse({'success': True})
+
+
